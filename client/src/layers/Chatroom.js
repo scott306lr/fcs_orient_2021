9f2d9181530a9c39ae528587d5146cc252b67acd
scott306lr <scott306lr@gmail.com>
2021-09-03 02:27:14 +0800

    chatroom ui

--- client/src/layers/Chatroom.js
+++ client/src/layers/Chatroom.js
@@ -3,22 +3,51 @@ import { io, Socket } from "socket.io-client";
 
 
 export default function Chatroom() {
+  const [messages, setMessages] = useState([]);
   const socket = useRef();
 
   useEffect(() => {
     socket.current = io("ws://localhost:8900");
     
     socket.current.emit("hi");
-    socket.current.on("welcome", (msg) => {
-      console.log(msg);
+    socket.current.on("got message", (payload) => {
+      setMessages(messages => [...messages, payload]);
+      console.log(payload);
     })
   }, []);
 
+  const msgJSON = [
+    {
+      time: "21:02",
+      name: "AA",
+      role: "Admin",
+      team: 1,
+      text: "Hello!"
+    },
+    {
+      time: "21:03",
+      name: "BB",
+      role: "Member",
+      team: 5,
+      text: "Hi!"
+    },{
+      time: "21:03",
+      name: "CC",
+      role: "Member",
+      team: 3,
+      text: "wwwwwww"
+    },
+  ];
 
+  const MessageList = msgJSON.map((msg) => {
+    return (
+      <li>
+        --{msg.role}-- {msg.name}: {msg.text}
+      </li>
+    );
+  });
 
   return (
-    <div>
-      Chatroom
-    </div>
+    <ul> { MessageList } </ul>
   );
 }

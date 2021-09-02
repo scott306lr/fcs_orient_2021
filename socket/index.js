0cad11af49b55d1904d519b33d082c04cd2b9bea
scott306lr <scott306lr@gmail.com>
2021-09-03 01:41:25 +0800

    socket server seems done.

--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,56 @@
+TEAM_COUNT = 6
+
 const io = require("socket.io")(8900, {
   cors: {
     origin: "http://localhost:3000"
   }
 });
 
+const Teams = [...Array(TEAM_COUNT).keys()].map(x => ++x) // [1, 2, 3, 4, 5, 6]
+console.log(Teams)
+
+var GameStatus = {
+  in_game: false, 
+  board_freeze: false,
+}
+
 io.on("connection", (socket) => {
-  console.log("a user connected.")
-  io.emit("welcome", "hello this is server!")
+  console.log("a user connected. qq")
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on("answer correct", ({ team, score, task }) => {
+    io.emit.to(team).emit("update task" ,task);
+    io.emit("update score", score);
+  })
+
+  socket.on("send message", ({ payload }) => {
+    io.emit("got message", payload);
+  });
+
+
+  //changing game status, for admin use only.
+  socket.on("start game", () => {
+    GameStatus.in_game = true;
+    io.emit("status update", GameStatus)
+  })
+
+  socket.on("end game", () => {
+    GameStatus.in_game = false;
+    io.emit("status update", GameStatus)
+  })
+
+  socket.on("freeze board", () => {
+    GameStatus.board_freeze = true;
+    io.emit("status update", GameStatus)
+  })
+
+  socket.on("unfreeze board", () => {
+    GameStatus.board_freeze = false;
+    io.emit("board unfreezed")
+  })
 })
+
+
 

bd86667a4889b3584c4ff98bc170b7f3b4f628ce
udchen19 <ueidarchen@gmail.com>
2021-09-02 16:10:14 +0800

    Submit

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,4 +1,7 @@
+import React, { useState } from 'react';
 export default function Task(props) {
+
+  const [answerText, setAnswer] = useState("");
 
   const task = {
     id: props.id,
@@ -6,13 +9,20 @@ export default function Task(props) {
     info: "This is a question."
   };
 
+  function answerCheck(answerText) {
+    if (answerText === "This is the answer.")
+      alert('YES!');
+    else
+      alert ('NO');
+  }
+
   const completeTask = (
     <div>
       <p>{task.info}</p>
       <img src = {`../assets/tasks/${task.id}.jpg`} alt = "Task Image"/>
       <br />
-      <input placeholder="請輸入答案" />
-      <button onClick = {() => alert('Submission made.')}>上傳</button>
+      <input placeholder="請輸入答案" value={answerText} onChange={e => setAnswer(e.target.value)}/>
+      <button onClick = {() => answerCheck(answerText)}>上傳</button>
     </div>
   );
 

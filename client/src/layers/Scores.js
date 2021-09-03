19b3cef8a03911e42abd3b9305178371799d93f2
yichi170 <ilee900620@gmail.com>
2021-09-03 13:34:47 +0800

    scoreboard add css

--- client/src/layers/Scores.js
+++ client/src/layers/Scores.js
@@ -3,11 +3,12 @@ import Scoreboard from "../components/Scoreboard";
 export default function Score(props) {
     const teamList = [
         {
-            teamID: "team1",
+            teamID: "一小",
             gold: 1,
             silver: 1,
             bronze: 1,
             iron: 1,
+            score: 10,
         },
         {
             teamID: "team2",
@@ -15,6 +16,7 @@ export default function Score(props) {
             silver: 2,
             bronze: 2,
             iron: 2,
+            score: 10,
         },
         {
             teamID: "team3",
@@ -22,19 +24,24 @@ export default function Score(props) {
             silver: 3,
             bronze: 3,
             iron: 3,
+            score: 10,
         },
     ];
     const iterTeam = teamList.map((teamJSON) => {
         return (
-          <div>
-            <Scoreboard team={teamJSON.teamID} gold={teamJSON.gold} silver={teamJSON.silver} bronze={teamJSON.bronze} iron={teamJSON.iron}/>
-          </div>
+            <div class="max-w-prose mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+                <div class="md:flex">
+                    <div class="p-8">
+                        <Scoreboard team={teamJSON.teamID} gold={teamJSON.gold} silver={teamJSON.silver} bronze={teamJSON.bronze} iron={teamJSON.iron} score={teamJSON.score}/>
+                    </div>
+                </div>
+            </div>                
         );
     });
     return (
-        <div>
+        <div class="m-4">
             <h3>計分榜</h3>
-            <div id="scoreList">
+            <div id="scoreList" class="space-y-4">
                 {iterTeam}
             </div>
         </div>

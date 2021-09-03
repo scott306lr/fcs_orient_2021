deb2fe64fd19bd84da968fa679fa9d46384570b3
udchen19 <ueidarchen@gmail.com>
2021-09-03 15:26:15 +0800

    state var of Member

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,20 +3,18 @@ import Member from "../layers/Member";
 import Chatroom from "../layers/Chatroom";
 
 export default function Home() {
-
-  const chat = (
-    <Chatroom />
-  );
+  const [chatOpen, setChat] = useState(false);
+  const [scoreOpen, setScore] = useState(false);
 
   return (
     <div>
       <h1>FCS ORIENTING 2021</h1>
       <div id = "topBar">
-        <button onClick = {() => openChat()}>Chatroom</button>
-        <button onClick = {() => openScore()}>Scoreboard</button>
+        <button onClick = {() => setChat(!chatOpen)}>Chatroom</button>
+        <button onClick = {() => setScore(!scoreOpen)}>Scoreboard</button>
       </div>
       <Member />
-      {chat}
+      <Chatroom />
     </div>
   );
 }

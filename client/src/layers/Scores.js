13a0e1ed2d34cea84450d5df275d7c086682e627
yichi170 <ilee900620@gmail.com>
2021-09-05 01:34:20 +0800

    modify scoreboard

--- client/src/layers/Scores.js
+++ client/src/layers/Scores.js
@@ -2,6 +2,10 @@ import { useContext, useEffect, useState } from "react";
 import Scoreboard from "../components/Scoreboard";
 import { AuthContext } from "../context/AuthContext";
 
+import gmadal from '../img/medal-1.png';
+import smadal from '../img/medal-2.png';
+import bmadal from '../img/medal-3.png';
+
 const teamsJSON = [
   {teamID: 1, name: "haha1", gold: "3", silver: "7", bronze: "3", iron: "2", score:"41"}, 
   {teamID: 2, name: "haha2", gold: "1", silver: "7", bronze: "3", iron: "2", score:"33"},
@@ -89,7 +93,23 @@ export default function Score(props) {
   
   return (
     <div class="p-4 w-full overflow-hidden">
-      <h2>計分榜</h2>
+      <h2 class="text-xl">計分榜</h2>
+      <div class="max-w-prose mx-auto p-8">
+        <div class="flex">
+          <div class="flex-1 text-center align-middle uppercase font-bold">team</div>
+          <div class="flex-1 text-center align-middle">
+            <img src={gmadal} alt="gold" class="h-8" />
+          </div>
+          <div class="flex-1 text-center align-middle">
+            <img src={smadal} alt="silver" class="h-8" />
+          </div>
+          <div class="flex-1 text-center align-middle">
+            <img src={bmadal} alt="bronze" class="h-8" />
+          </div>
+          <div class="flex-1 text-center align-middle">iron</div>
+          <div class="flex-1 text-center align-middle">score</div>
+        </div>
+      </div>
       <div id="scoreList" class="space-y-4">
         {iterTeam}
       </div>

f644960aadb04cd2863f87d435030a7de461332e
scott306lr <scott306lr@gmail.com>
2021-09-04 10:16:35 +0800

    adding socketio to score

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -1,12 +1,12 @@
 export default function ScoreBoard(props) {
-    return (
-        <div class="flex">
-            <div class="flex-1 text-center align-middle uppercase font-bold">{props.team}</div>
-            <div class="flex-1 text-center align-middle">{props.gold}</div>
-            <div class="flex-1 text-center align-middle">{props.silver}</div>
-            <div class="flex-1 text-center align-middle">{props.bronze}</div>
-            <div class="flex-1 text-center align-middle">{props.iron}</div>
-            <div class="flex-1 text-center align-middle">{props.score}</div>
-        </div>
-    );
+  return (
+    <div class="flex">
+      <div class="flex-1 text-center align-middle uppercase font-bold">{props.team}</div>
+      <div class="flex-1 text-center align-middle">{props.gold}</div>
+      <div class="flex-1 text-center align-middle">{props.silver}</div>
+      <div class="flex-1 text-center align-middle">{props.bronze}</div>
+      <div class="flex-1 text-center align-middle">{props.iron}</div>
+      <div class="flex-1 text-center align-middle">{props.score}</div>
+    </div>
+  );
 }
--- client/src/layers/Scores.js
+++ client/src/layers/Scores.js
@@ -1,49 +1,102 @@
 import Scoreboard from "../components/Scoreboard";
 
+const teamList = [
+    {
+        teamID: "一小",
+        gold: 1,
+        silver: 1,
+        bronze: 1,
+        iron: 1,
+        score: 10,
+    },
+    {
+        teamID: "team2",
+        gold: 2,
+        silver: 2,
+        bronze: 2,
+        iron: 2,
+        score: 10,
+    },
+    {
+        teamID: "team3",
+        gold: 3,
+        silver: 3,
+        bronze: 3,
+        iron: 3,
+        score: 10,
+    },
+];
+
+const taskDone = [
+  {
+    time:"21:34",
+    who:"LR",
+    team:1,
+    taskID:9,
+    score:4,
+  },
+  {
+    time:"21:35",
+    who:"WJ",
+    team:2,
+    taskID:8,
+    score:4,
+  },
+  {
+    time:"21:34",
+    who:"LR",
+    team:1,
+    taskID:8,
+    score:3,
+  },
+  {
+    time:"21:34",
+    who:"LR",
+    team:1,
+    taskID:3,
+    score:4,
+  },
+  {
+    time:"21:34",
+    who:"UD",
+    team:3,
+    taskID:3,
+    score:3,
+  },
+  {
+    time:"21:34",
+    who:"WJ",
+    team:2,
+    taskID:9,
+    score:3,
+  },
+  {
+    time:"21:34",
+    who:"YC",
+    team:4,
+    taskID:3,
+    score:2,
+  },
+]
+
 export default function Score(props) {
-    const teamList = [
-        {
-            teamID: "一小",
-            gold: 1,
-            silver: 1,
-            bronze: 1,
-            iron: 1,
-            score: 10,
-        },
-        {
-            teamID: "team2",
-            gold: 2,
-            silver: 2,
-            bronze: 2,
-            iron: 2,
-            score: 10,
-        },
-        {
-            teamID: "team3",
-            gold: 3,
-            silver: 3,
-            bronze: 3,
-            iron: 3,
-            score: 10,
-        },
-    ];
-    const iterTeam = teamList.map((teamJSON) => {
-        return (
-            <div class="max-w-prose mx-auto bg-white rounded-xl shadow-md hover:shadow-xl overflow-hidden">
-                {/* <div class="md:flex"> */}
-                    <div class="p-8">
-                        <Scoreboard team={teamJSON.teamID} gold={teamJSON.gold} silver={teamJSON.silver} bronze={teamJSON.bronze} iron={teamJSON.iron} score={teamJSON.score}/>
-                    </div>
-                {/* </div> */}
-            </div>                
-        );
-    });
+  const iterTeam = teamList.map((teamJSON) => {
     return (
-        <div class={props.opened ? "m-4 w-1/2": "m-4"}>
-            <h2>計分榜</h2>
-            <div id="scoreList" class="space-y-4">
-                {iterTeam}
-            </div>
-        </div>
+      <div class="max-w-prose mx-auto bg-white rounded-xl shadow-md hover:shadow-xl overflow-hidden">
+        {/* <div class="md:flex"> */}
+          <div class="p-8">
+            <Scoreboard team={teamJSON.teamID} gold={teamJSON.gold} silver={teamJSON.silver} bronze={teamJSON.bronze} iron={teamJSON.iron} score={teamJSON.score}/>
+          </div>
+        {/* </div> */}
+      </div>                
     );
+  });
+  return (
+    <div class={props.opened ? "m-4 w-1/2": "m-4"}>
+      <h2>計分榜</h2>
+      <div class="space-y-4">
+        {iterTeam}
+      </div>
+    </div>
+  );
 }

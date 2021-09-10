50032bbbecd453018ffcb93d4cdf758bbc96ad51
scott306lr <scott306lr@gmail.com>
2021-09-10 12:11:42 +0800

    mend
    changed way to axios

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const PREFIX = '/backend'
+
 dotenv.config();
 
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
@@ -21,17 +23,17 @@ app.use(helmet());
 app.use(morgan("common"));
 
 const userRouter = require("./routes/user");
-app.use("/api/auth", userRouter);
+app.use(PREFIX+"/auth", userRouter);
 const taskRouter = require("./routes/task");
-app.use("/api/task", taskRouter);
+app.use(PREFIX+"/task", taskRouter);
 const messageRouter = require("./routes/message");
-app.use("/api/message", messageRouter);
+app.use(PREFIX+"/message", messageRouter);
 const doneTaskRouter = require("./routes/doneTask");
-app.use("/api/doneTask", doneTaskRouter);
+app.use(PREFIX+"/doneTask", doneTaskRouter);
 const teamTaskRouter = require("./routes/teamTask");
-app.use("/api/teamTask", teamTaskRouter);
+app.use(PREFIX+"/teamTask", teamTaskRouter);
 const teamRouter = require("./routes/team");
-app.use("/api/team", teamRouter);
+app.use(PREFIX+"/team", teamRouter);
 
 app.listen(8000,()=>{
     console.log("Backend server is running");
